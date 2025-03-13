70b475d658f15ce48dc551cd6dc8ebdef8e827f6
Heyuan Zeng <hi@universe.observer>
2025-03-13 18:10:55 +0000

    Initial Commit

---
+++ BullittSdkDemo/BullittSdkDemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  BullittSdkDemoApp.swift
+//  BullittSdkDemo
+//
+//  Created by Larry Zeng on 3/13/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BullittSdkDemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BullittSdkDemo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BullittSdkDemo
+//
+//  Created by Larry Zeng on 3/13/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
